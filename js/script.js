79d3d5a2790796c2c52fea97384ca9184671db8a
MichalPruszynski7 <mpruszynski@hawk.iit.edu>
2023-11-21 18:10:04 -0600

    java

---
+++ js/script.js
@@ -0,0 +1,10 @@
+ function initMap() {
+   var map = new google.maps.Map(document.getElementById('map'), {
+     zoom: 13,
+     center: {lat: 52.2296756, lng: 21.0122287} 
+   });
+   var marker = new google.maps.Marker({
+     position: {lat: 52.2296756, lng: 21.0122287}, 
+     map: map
+   });
+}
